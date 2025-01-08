374035067b45aad277e0603f8ec39b8ad4c4e566
spho <hoszephing@gmail.com>
2025-01-08 09:49:56 -0500

    update files

--- src/main/java/com/example/messageapp/MessageApplication.java
+++ src/main/java/com/example/messageapp/MessageApplication.java
@@ -3,7 +3,7 @@ package com.example.messageapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// https://employee1-447012.ue.r.appspot.com/
+// https://employee1-447012.ue.r.appspot.com/<endpoint>/<param>
 // http://localhost:8081/swagger-ui.html
 // http://localhost:8081/messageService/<endpoint>/<param>
 @SpringBootApplication
