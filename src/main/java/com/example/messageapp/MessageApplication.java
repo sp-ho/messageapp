c28f01636baf4294da5bc6cf1831c5517294c06f
sp-ho <106715796+sp-ho@users.noreply.github.com>
2025-01-08 16:44:09 -0500

    Update MessageApplication.java

--- src/main/java/com/example/messageapp/MessageApplication.java
+++ src/main/java/com/example/messageapp/MessageApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // https://employee1-447012.ue.r.appspot.com/<endpoint>/<param>
-// http://localhost:8081/swagger-ui.html
-// http://localhost:8081/messageService/<endpoint>/<param>
 @SpringBootApplication
 public class MessageApplication {
 
